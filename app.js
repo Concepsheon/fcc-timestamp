9f0bde1042f506ce6539188eaee6b3f783ac3558
Concepsheon <raine20137@gmail.com>
2016-05-06 15:41:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var host = process.env.IP || 'localhost';
 app.use(morgan('dev'));
 
 app.get('/', function(req,res){
-    res.send('<h1>Example</h1><div><h4>Input</h4><p>localhost:3000/771292800,</p><p>localhost:3000/June%2011%201994</p><h4>Output</h4><p>{"unix": "771292800", "natural": "11 Jun 1994"}</p></div>');
+    res.send('<h1>Example</h1><div><h4>Input</h4><p>localhost:3000/771292800</p><p>localhost:3000/June%2011%201994</p><h4>Output</h4><p>{"unix": "771292800", "natural": "11 Jun 1994"}</p></div>');
 });
 
 app.get('/:time', function(req, res){
