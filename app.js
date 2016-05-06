30d3c009d6b83b043a7d980e37ef00ff8d46ac25
Concepsheon <raine20137@gmail.com>
2016-05-06 15:36:12 -0400

    Change url

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ var ts = require("readable-timestamp");
 var unix = require("to-unix-timestamp");
 
 var app = express();
-var port = process.env.PORT;
-var host = process.env.IP;
+var port = process.env.PORT || 3000;
+var host = process.env.IP || 'localhost';
 
 
 app.use(morgan('dev'));
 
 app.get('/', function(req,res){
-    res.send('<h1>Example</h1><div><h4>Input</h4><p>https://mongodb-concepsheon.c9users.io:8080/771292800,</p><p>https://mongodb-concepsheon.c9users.io:8080/June%2011%201994</p><h4>Output</h4><p>{"unix": "771292800", "natural": "11 Jun 1994"}</p></div>');
+    res.send('<h1>Example</h1><div><h4>Input</h4><p>localhost:3000/771292800,</p><p>localhost:3000/June%2011%201994</p><h4>Output</h4><p>{"unix": "771292800", "natural": "11 Jun 1994"}</p></div>');
 });
 
 app.get('/:time', function(req, res){
@@ -19,6 +19,7 @@ app.get('/:time', function(req, res){
     if(+time >= 0){
         res.json({
             unix: time,
+            //convert timestamp to Date object and multiply by 1000 so argument is in miliseconds
             natural: ts(new Date(time * 1000))
         });
     } else {
